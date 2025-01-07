89c2fe24f4e5283a489a3d5da9c6269cc9e74e79
Tiago Gomes <tidgomes@gmail.com>
2025-01-07 11:11:41 -0300

    test: returns 400 if the description field is an empty string

--- backend/src/controller/NewTodo.spec.ts
+++ backend/src/controller/NewTodo.spec.ts
@@ -64,4 +64,20 @@ describe("NewTodoController", () => {
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual("Invalid param: title");
   });
+
+  it("should return 400 if the description is an empty string", () => {
+    const sut = new NewTodo();
+
+    const httpRequest = {
+      body: {
+        title: "any_title",
+        description: "  ",
+        done: false,
+      },
+    };
+    const response = sut.handle(httpRequest);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual("Invalid param: description");
+  });
 });
