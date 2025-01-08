159f5ed7cd2b34c0fb8f2bdcd930e9883a7aac27
Tiago Gomes <tidgomes@gmail.com>
2025-01-08 10:18:11 -0300

    test: calls the save method from the todoRepository once

--- backend/src/controller/NewTodo.spec.ts
+++ backend/src/controller/NewTodo.spec.ts
@@ -1,8 +1,21 @@
+import { MongoTodoRepository } from "../repositories/MongoTodoRepository";
+import { TodoRepository } from "../repositories/TodoRepository";
 import { NewTodo } from "./NewTodo";
 
+type SutTypes = {
+  sut: NewTodo;
+  todoRepositoryStub: TodoRepository;
+};
+
+const makeSut = (): SutTypes => {
+  const todoRepositoryStub = new MongoTodoRepository();
+  const sut = new NewTodo(todoRepositoryStub);
+  return { sut, todoRepositoryStub };
+};
+
 describe("NewTodoController", () => {
-  it("should return 400 if no title is provided", () => {
-    const sut = new NewTodo();
+  it("should return 400 if no title is provided", async () => {
+    const { sut } = makeSut();
 
     const httpRequest = {
       body: {
@@ -11,14 +24,14 @@ describe("NewTodoController", () => {
       },
     };
 
-    const response = sut.handle(httpRequest);
+    const response = await sut.handle(httpRequest);
 
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual("Missing param: title");
   });
 
-  it("should return 400 if no task description is provided", () => {
-    const sut = new NewTodo();
+  it("should return 400 if no task description is provided", async () => {
+    const { sut } = makeSut();
 
     const httpRequest = {
       body: {
@@ -27,14 +40,14 @@ describe("NewTodoController", () => {
       },
     };
 
-    const response = sut.handle(httpRequest);
+    const response = await sut.handle(httpRequest);
 
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual("Missing param: description");
   });
 
-  it("should return 400 if no status of done is provided", () => {
-    const sut = new NewTodo();
+  it("should return 400 if no status of done is provided", async () => {
+    const { sut } = makeSut();
 
     const httpRequest = {
       body: {
@@ -43,14 +56,14 @@ describe("NewTodoController", () => {
       },
     };
 
-    const response = sut.handle(httpRequest);
+    const response = await sut.handle(httpRequest);
 
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual("Missing param: done");
   });
 
-  it("should return 400 if the title is an empty string", () => {
-    const sut = new NewTodo();
+  it("should return 400 if the title is an empty string", async () => {
+    const { sut } = makeSut();
 
     const httpRequest = {
       body: {
@@ -59,14 +72,14 @@ describe("NewTodoController", () => {
         done: false,
       },
     };
-    const response = sut.handle(httpRequest);
+    const response = await sut.handle(httpRequest);
 
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual("Invalid param: title");
   });
 
-  it("should return 400 if the description is an empty string", () => {
-    const sut = new NewTodo();
+  it("should return 400 if the description is an empty string", async () => {
+    const { sut } = makeSut();
 
     const httpRequest = {
       body: {
@@ -75,9 +88,25 @@ describe("NewTodoController", () => {
         done: false,
       },
     };
-    const response = sut.handle(httpRequest);
+    const response = await sut.handle(httpRequest);
 
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual("Invalid param: description");
+  });
+
+  it("call the method to save the Todo", async () => {
+    const { sut, todoRepositoryStub } = makeSut();
+
+    const todoSaveSpy = jest.spyOn(todoRepositoryStub, "save");
+    const httpRequest = {
+      body: {
+        title: "any_title",
+        description: "any_description",
+        done: false,
+      },
+    };
+    await sut.handle(httpRequest);
+
+    expect(todoSaveSpy).toHaveBeenCalledTimes(1);
   });
 });
