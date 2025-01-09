bb3efa55698cb174b8bcccb6672ef56afadd44e9
Tiago Gomes <tidgomes@gmail.com>
2025-01-09 10:51:55 -0300

    feat: connect to the db

--- backend/src/main/server.ts
+++ backend/src/main/server.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
 
 app.listen(3000, () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("connected to the db"))
+    .catch((err) => console.log(err));
   console.log(`Server running on port 3000`);
 });
