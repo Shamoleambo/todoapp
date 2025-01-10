3e4fb27fc762dbe1e4af0b4cfe9ed9573038d682
Tiago Gomes <tidgomes@gmail.com>
2025-01-10 10:24:11 -0300

    refactor: change the port to be retrieved by the .env file

--- backend/src/main/server.ts
+++ backend/src/main/server.ts
@@ -9,10 +9,10 @@ app.use(json());
 
 app.use(router);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log("connected to the db"))
     .catch((err) => console.log(err));
-  console.log(`Server running on port 3000`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
