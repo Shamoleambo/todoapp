ec146deb84f0a18da20fc3cad303857b811c5313
Tiago Gomes <tidgomes@gmail.com>
2025-01-10 15:02:53 -0300

    test: should return all todos on 200

--- backend/src/controller/GetAllTodos.spec.ts
+++ backend/src/controller/GetAllTodos.spec.ts
@@ -1,3 +1,4 @@
+import { Todo } from "../models/Todo";
 import { MongoTodoRepository } from "../repositories/MongoTodoRepository";
 import { TodoRepository } from "../repositories/TodoRepository";
 import { Controller } from "./Controller";
@@ -13,6 +14,21 @@ const makeSut = (): SutTypes => {
   const sut = new GetAllTodos(todoRepository);
   return { sut, todoRepository };
 };
+
+const TODOS_DUMMY: Partial<Todo>[] = [
+  {
+    _id: "any_id_1",
+    title: "first_todo",
+    description: "_first_todo",
+    done: false,
+  },
+  {
+    _id: "any_id_1",
+    title: "second_todo",
+    description: "second_todo",
+    done: false,
+  },
+];
 
 describe("GetAllTodos Controller", () => {
   it("should call the method findAll from the todoRepository once", async () => {
@@ -36,5 +52,18 @@ describe("GetAllTodos Controller", () => {
 
     expect(response.statusCode).toBe(500);
     expect(response.body).toEqual("Cannot access database");
+  });
+
+  it("should return all todos on 200", async () => {
+    const { sut, todoRepository } = makeSut();
+
+    jest
+      .spyOn(todoRepository, "findAll")
+      .mockResolvedValueOnce(TODOS_DUMMY as Todo[]);
+    const httpRequest = {};
+    const response = await sut.handle(httpRequest);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(TODOS_DUMMY);
   });
 });
