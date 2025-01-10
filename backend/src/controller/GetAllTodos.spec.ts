8e5beccfd1af708a646fa2d0a2cb5db993040dc5
Tiago Gomes <tidgomes@gmail.com>
2025-01-10 14:17:23 -0300

    test: returns 500 if database cannot be accessed

---
+++ backend/src/controller/GetAllTodos.spec.ts
@@ -0,0 +1,30 @@
+import { MongoTodoRepository } from "../repositories/MongoTodoRepository";
+import { TodoRepository } from "../repositories/TodoRepository";
+import { Controller } from "./Controller";
+import { GetAllTodos } from "./GetAllTodos";
+
+type SutTypes = {
+  sut: Controller;
+  todoRepository: TodoRepository;
+};
+
+const makeSut = (): SutTypes => {
+  const todoRepository = new MongoTodoRepository();
+  const sut = new GetAllTodos(todoRepository);
+  return { sut, todoRepository };
+};
+
+describe("GetAllTodos Controller", () => {
+  it("should return 500 if db cannot be accessed", async () => {
+    const { sut, todoRepository } = makeSut();
+
+    jest
+      .spyOn(todoRepository, "findAll")
+      .mockRejectedValueOnce(new Error("Cannot access database"));
+    const httpRequest = {};
+    const response = await sut.handle(httpRequest);
+
+    expect(response.statusCode).toBe(500);
+    expect(response.body).toEqual("Cannot access database");
+  });
+});
