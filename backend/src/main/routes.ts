31acf768fc61cde322144a1971dbcf3af4ec1373
Tiago Gomes <tidgomes@gmail.com>
2025-01-13 10:36:34 -0300

    feat: add a new route to retrieve all the todos

--- backend/src/main/routes.ts
+++ backend/src/main/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { adaptController } from "./controllerAdapter";
 import { makeCreateTodoController } from "./controllerFactories/CreateTodoFactory";
+import { makeGetAllTodos } from "./controllerFactories/GetAllTodosFactory";
 
 const router = Router();
 
 router.post("/api/create-todo", adaptController(makeCreateTodoController()));
+router.get("/api/todos", adaptController(makeGetAllTodos()));
 
 export default router;
