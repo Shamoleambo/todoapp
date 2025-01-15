c0a93dd2164b5a62036c398380678f298bee963a
Tiago Gomes <tidgomes@gmail.com>
2025-01-15 14:20:55 -0300

    test: return 400 if no description field is provided

--- backend/src/controller/EditTodo.spec.ts
+++ backend/src/controller/EditTodo.spec.ts
@@ -56,6 +56,23 @@ describe("EditTodoController", () => {
     expect(response.body).toEqual(`Missing param: title`);
   });
 
+  it("should return 400 if no description is provided", async () => {
+    const { sut } = makeSut();
+
+    const anyId = new mongoose.Types.ObjectId().toString();
+    const httpRequest = {
+      params: { id: anyId },
+      body: {
+        title: "any_title",
+        done: false,
+      },
+    };
+    const response = await sut.handle(httpRequest);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual("Missing param: description");
+  });
+
   it("should return 400 if the title is blank space string", async () => {
     const { sut } = makeSut();
 
