66da00f635bfc48a02a9f2359f8f83c3859c8981
Tiago Gomes <tidgomes@gmail.com>
2025-01-15 09:05:15 -0300

    test: calls todoRepository findById with correct id

--- backend/src/controller/EditTodo.spec.ts
+++ backend/src/controller/EditTodo.spec.ts
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
+import { TodoRepository } from "../repositories/TodoRepository";
 import { Controller } from "./Controller";
 import { EditTodo } from "./EditTodo";
 
 describe("EditTodoController", () => {
   type SutTypes = {
     sut: Controller;
+    todoRepository: TodoRepository;
   };
 
   const makeSut = (): SutTypes => {
-    const sut = new EditTodo();
-    return { sut };
+    const todoRepository = {
+      findById: jest.fn(),
+    } as unknown as TodoRepository;
+    const sut = new EditTodo(todoRepository);
+    return { sut, todoRepository };
   };
 
   it("should return 400 if id is not found", async () => {
@@ -30,5 +35,26 @@ describe("EditTodoController", () => {
 
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual(`No Todo found with id: ${invalidId}`);
+  });
+
+  it("should call todoRepository findById and call it with correct id", async () => {
+    const { sut, todoRepository } = makeSut();
+
+    const findByIdSpy = jest.spyOn(todoRepository, "findById");
+    const validId = new mongoose.Types.ObjectId().toString();
+    const httpRequest = {
+      params: {
+        id: validId,
+      },
+      body: {
+        title: "any_title",
+        description: "any_description",
+        done: false,
+      },
+    };
+    await sut.handle(httpRequest);
+
+    expect(findByIdSpy).toHaveBeenCalledTimes(1);
+    expect(findByIdSpy).toHaveBeenCalledWith(validId);
   });
 });
