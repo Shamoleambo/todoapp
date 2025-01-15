733511ff2ed815d2c7bb22d1d7dffdcf2035c0bc
Tiago Gomes <tidgomes@gmail.com>
2025-01-15 14:26:35 -0300

    test: return 400 if no param done is provided

--- backend/src/controller/EditTodo.spec.ts
+++ backend/src/controller/EditTodo.spec.ts
@@ -73,6 +73,23 @@ describe("EditTodoController", () => {
     expect(response.body).toEqual("Missing param: description");
   });
 
+  it("should return 400 if no done field is provided", async () => {
+    const { sut } = makeSut();
+
+    const anyId = new mongoose.Types.ObjectId().toString();
+    const httpRequest = {
+      params: { id: anyId },
+      body: {
+        title: "any_title",
+        description: "any_description",
+      },
+    };
+    const response = await sut.handle(httpRequest);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual("Missing param: done");
+  });
+
   it("should return 400 if the title is blank space string", async () => {
     const { sut } = makeSut();
 
