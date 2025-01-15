3066b1c61ed2c85d1a7bf26f432c728af026f20b
Tiago Gomes <tidgomes@gmail.com>
2025-01-15 09:19:02 -0300

    feat: returns 400 if the title is a blank space string

--- backend/src/controller/EditTodo.ts
+++ backend/src/controller/EditTodo.ts
@@ -1,3 +1,4 @@
+import { badRequest } from "../helpers/httResponse";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 import { TodoRepository } from "../repositories/TodoRepository";
 import { Controller } from "./Controller";
@@ -12,6 +13,10 @@ export class EditTodo implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       await this.todoRepository.findById(httpRequest.params.id);
+
+      if (!httpRequest.body.title.trim()) {
+        return badRequest("Invalid field: title");
+      }
       return {
         statusCode: 400,
         body: `No Todo found with id: ${httpRequest.params.id}`,
