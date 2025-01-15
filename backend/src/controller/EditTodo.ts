686821955520af024187bc75699c533954410ce3
Tiago Gomes <tidgomes@gmail.com>
2025-01-15 09:05:06 -0300

    feat: calls todoRepository findById with correct id

--- backend/src/controller/EditTodo.ts
+++ backend/src/controller/EditTodo.ts
@@ -1,8 +1,16 @@
 import { HttpRequest, HttpResponse } from "../protocols/http";
+import { TodoRepository } from "../repositories/TodoRepository";
 import { Controller } from "./Controller";
 
 export class EditTodo implements Controller {
+  private readonly todoRepository: TodoRepository;
+
+  constructor(todoRepository: TodoRepository) {
+    this.todoRepository = todoRepository;
+  }
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.todoRepository.findById(httpRequest.params.id);
     return {
       statusCode: 400,
       body: `No Todo found with id: ${httpRequest.params.id}`,
