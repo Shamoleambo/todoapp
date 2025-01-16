ecc0ea788eb778c55b558c5837ab262a111ea17a
Tiago Gomes <tidgomes@gmail.com>
2025-01-16 14:35:17 -0300

    test: calls todoRepository's findById with the correct param

--- backend/src/controller/DeleteTodo.spec.ts
+++ backend/src/controller/DeleteTodo.spec.ts
@@ -1,13 +1,34 @@
+import { Todo } from "../models/Todo";
+import { TodoRepository } from "../repositories/TodoRepository";
 import { Controller } from "./Controller";
 import { DeleteTodo } from "./DeleteTodo";
 
 describe("DeleteTodo Controller", () => {
-  const makeSut = (): Controller => {
-    return new DeleteTodo();
+  const TODO_DUMMY = {
+    _id: "valid_id",
+    title: "any_title",
+    description: "any_description",
+    done: false,
+  } as unknown as Todo;
+
+  type SutTypes = {
+    sut: Controller;
+    todoRepository: TodoRepository;
+  };
+
+  const makeSut = (): SutTypes => {
+    const todoRepository = {
+      findById: jest.fn().mockImplementationOnce((id: string) => {
+        if (id === "valid_id") return Promise.resolve(TODO_DUMMY);
+        return Promise.resolve(null);
+      }),
+    } as unknown as TodoRepository;
+    const sut = new DeleteTodo(todoRepository);
+    return { sut, todoRepository };
   };
 
   it("should return 400 if no Todo is found with provided id", async () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
 
     const httpRequest = {
       params: {
@@ -18,5 +39,20 @@ describe("DeleteTodo Controller", () => {
 
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual("No Todo found with id: invalid_id");
+  });
+
+  it("should call todoRepository findById once and with the correct param", async () => {
+    const { sut, todoRepository } = makeSut();
+
+    const findByIdSpy = jest.spyOn(todoRepository, "findById");
+    const httpRequest = {
+      params: {
+        id: "valid_id",
+      },
+    };
+    await sut.handle(httpRequest);
+
+    expect(findByIdSpy).toHaveBeenCalledTimes(1);
+    expect(findByIdSpy).toHaveBeenCalledWith("valid_id");
   });
 });
