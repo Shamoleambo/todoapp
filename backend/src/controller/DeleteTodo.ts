e9b85b3e31e983290a8f4c6c4aa35684dde23786
Tiago Gomes <tidgomes@gmail.com>
2025-01-16 14:35:05 -0300

    feat: calls todoRepository's findById with the correct param

--- backend/src/controller/DeleteTodo.ts
+++ backend/src/controller/DeleteTodo.ts
@@ -1,9 +1,18 @@
 import { badRequest } from "../helpers/httResponse";
 import { HttpRequest, HttpResponse } from "../protocols/http";
+import { TodoRepository } from "../repositories/TodoRepository";
 import { Controller } from "./Controller";
 
 export class DeleteTodo implements Controller {
+  private readonly todoRepository: TodoRepository;
+
+  constructor(todoRepository: TodoRepository) {
+    this.todoRepository = todoRepository;
+  }
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const { id } = httpRequest.params;
+    await this.todoRepository.findById(id);
     return badRequest(`No Todo found with id: ${httpRequest.params.id}`);
   }
 }
