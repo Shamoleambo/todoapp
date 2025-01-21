56ac09e24c4536c056372705c901c1bcef1e33a2
Tiago Gomes <tidgomes@gmail.com>
2025-01-21 08:58:16 -0300

    feat: make NewTodo component a controlled component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import "./App.css";
+import { useState } from "react";
 import NewTodo from "./sections/NewTodo";
+import "./App.css";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <div>
-      <NewTodo />
+      <NewTodo setTitle={setTitle} setDescription={setDescription} />
     </div>
   );
 }
--- frontend/src/sections/NewTodo.tsx
+++ frontend/src/sections/NewTodo.tsx
@@ -1,17 +1,42 @@
 import styles from "./NewTodo.module.css";
 
-const NewTodo = () => {
+const NewTodo: React.FC<{
+  setTitle: (title: string) => void;
+  setDescription: (description: string) => void;
+}> = ({ setTitle, setDescription }) => {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(e.target.value);
+  };
+
   return (
     <div className={styles.container}>
       <h1>New Todo</h1>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles["field-container"]}>
           <label htmlFor="title">Title: </label>
-          <input type="text" id="title" name="title" />
+          <input
+            type="text"
+            id="title"
+            name="title"
+            onChange={handleTitleChange}
+          />
         </div>
         <div className={styles["field-container"]}>
           <label htmlFor="description">Description: </label>
-          <input type="text" id="description" name="description" />
+          <input
+            type="text"
+            id="description"
+            name="description"
+            onChange={handleDescriptionChange}
+          />
         </div>
         <button type="submit">Create</button>
       </form>
