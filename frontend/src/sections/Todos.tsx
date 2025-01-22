d3f24c92dafcc1af70c7976799b7863d4a92ae12
Tiago Gomes <tidgomes@gmail.com>
2025-01-22 14:20:14 -0300

    feat: make the request to retrieve all of the todos and display them

--- frontend/src/sections/Todos.tsx
+++ frontend/src/sections/Todos.tsx
@@ -1,10 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Todo } from "../models/Todo";
 
 const Todos: React.FC = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleRequestForTodos = async () => {
+    const response = await axios.get("http://localhost:8080/api/todos");
+    setTodos(response.data);
+  };
+
+  useEffect(() => {
+    handleRequestForTodos();
+  }, []);
+
   return (
     <div>
       <h2>Todos</h2>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>{todo.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
