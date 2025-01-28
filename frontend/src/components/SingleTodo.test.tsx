1388e7fae808c053a34365df024146368959f52c
Tiago Gomes <tidgomes@gmail.com>
2025-01-28 12:23:11 -0300

    test: renders a Delete button

--- frontend/src/components/SingleTodo.test.tsx
+++ frontend/src/components/SingleTodo.test.tsx
@@ -18,17 +18,21 @@ describe("SingleTodo Component", () => {
     expect(screen.getByText("any_description")).toBeInTheDocument();
   });
 
-  it("renders a button for done and edit", () => {
+  it("renders a button for done, edit and delete", () => {
     render(<SingleTodo todo={mockTodo} done={false} />);
 
     const doneButton = screen.getByText("Done");
     const editButton = screen.getByText("Edit");
+    const deleteButton = screen.getByText("Delete");
 
     expect(doneButton).toBeInTheDocument();
     expect(editButton).toBeInTheDocument();
+    expect(deleteButton).toBeInTheDocument();
     expect(doneButton).toBeEnabled();
     expect(editButton).toBeEnabled();
+    expect(deleteButton).toBeEnabled();
     expect(doneButton).toHaveAttribute("type", "button");
     expect(editButton).toHaveAttribute("type", "button");
+    expect(deleteButton).toHaveAttribute("type", "button");
   });
 });
