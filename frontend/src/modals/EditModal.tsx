9fed2d8bd96c1e68bf5e9f44d0a10a8d903ecbd7
Tiago Gomes <tidgomes@gmail.com>
2025-01-31 14:09:45 -0300

    feat: transform the EditModal modal component into a controlled component

--- frontend/src/modals/EditModal.tsx
+++ frontend/src/modals/EditModal.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Modal from "../components/Modal";
 import { Todo } from "../models/Todo";
 
@@ -7,8 +8,20 @@ type EditModalProps = {
 };
 
 const EditModal: React.FC<EditModalProps> = ({ todo, setShowModal }) => {
+  const [title, setTitle] = useState(todo.title);
+  const [description, setDescription] = useState(todo.description);
+  const [done, setDone] = useState(todo.done);
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+  };
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(e.target.value);
   };
 
   return (
@@ -17,15 +30,30 @@ const EditModal: React.FC<EditModalProps> = ({ todo, setShowModal }) => {
       <div className="formContainer">
         <form>
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" value={todo.title} />
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={title}
+            onChange={handleTitleChange}
+          />
           <label htmlFor="description">Description</label>
           <input
             type="text"
             id="description"
             name="description"
-            value={todo.description}
+            value={description}
+            onChange={handleDescriptionChange}
           />
-          <input type="checkbox" id="done" name="done" checked={todo.done} />
+          <input
+            type="checkbox"
+            id="done"
+            name="done"
+            checked={done}
+            onClick={() => {
+              setDone((prevState) => !prevState);
+            }}
+          />
           <label htmlFor="done">Done</label>
           <div className="buttonsContainer">
             <button type="submit" onClick={handleSubmit}>
