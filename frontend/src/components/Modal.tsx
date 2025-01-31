2362d0ff0610b70a0f55c6fce901e011c4afb11e
Tiago Gomes <tidgomes@gmail.com>
2025-01-31 12:55:51 -0300

    refactor: create ModalProps type for Modal component

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -2,10 +2,12 @@ import { createPortal } from "react-dom";
 import { ReactNode } from "react";
 import classes from "./Modal.module.css";
 
-const Modal: React.FC<{
+type ModalProps = {
   children: ReactNode;
   setModal: (show: boolean) => void;
-}> = ({ children, setModal }) => {
+};
+
+const Modal: React.FC<ModalProps> = ({ children, setModal }) => {
   return createPortal(
     <>
       <div className={classes.backdrop} onClick={() => setModal(false)} />
