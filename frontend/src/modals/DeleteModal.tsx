ab361319042a3e977798286e9f067b7d24d5aecd
Tiago Gomes <tidgomes@gmail.com>
2025-01-31 09:17:35 -0300

    feat: add styling to the DeleteModal

--- frontend/src/modals/DeleteModal.tsx
+++ frontend/src/modals/DeleteModal.tsx
@@ -1,14 +1,17 @@
 import Modal from "../components/Modal";
+import classes from "./DeleteModal.module.css";
 
 const DeleteModal: React.FC<{ onClick: (show: boolean) => void }> = ({
   onClick,
 }) => {
   return (
     <Modal>
-      <h1>Hello Delete Modal</h1>
-      <div className="confirm-buttons-container">
-        <button onClick={() => onClick(false)}>Yes</button>
-        <button onClick={() => onClick(false)}>No</button>
+      <div className={classes.container}>
+        <h3>Are you sure you want to delete this task?</h3>
+        <div className={classes.buttonsContainer}>
+          <button onClick={() => onClick(false)}>Yes</button>
+          <button onClick={() => onClick(false)}>No</button>
+        </div>
       </div>
     </Modal>
   );
