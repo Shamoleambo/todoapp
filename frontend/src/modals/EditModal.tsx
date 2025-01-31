7340560855734e6409781ad500a9fbdc95d0326b
Tiago Gomes <tidgomes@gmail.com>
2025-01-31 13:13:42 -0300

    feat: create basic form structure of the Todo update modal

--- frontend/src/modals/EditModal.tsx
+++ frontend/src/modals/EditModal.tsx
@@ -7,9 +7,34 @@ type EditModalProps = {
 };
 
 const EditModal: React.FC<EditModalProps> = ({ todo, setShowModal }) => {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <Modal setModal={setShowModal}>
-      <h3>Edit Modal: {todo.title}</h3>
+      <h3>Edit</h3>
+      <div className="formContainer">
+        <form>
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" name="title" value={todo.title} />
+          <label htmlFor="description">Description</label>
+          <input
+            type="text"
+            id="description"
+            name="description"
+            value={todo.description}
+          />
+          <input type="checkbox" id="done" name="done" checked={todo.done} />
+          <label htmlFor="done">Done</label>
+          <div className="buttonsContainer">
+            <button type="submit" onClick={handleSubmit}>
+              Ok
+            </button>
+            <button onClick={() => setShowModal(false)}>Cancel</button>
+          </div>
+        </form>
+      </div>
     </Modal>
   );
 };
