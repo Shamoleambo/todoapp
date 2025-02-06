70556bd8da97223518ac9b70c68456bec25f9c13
Tiago Gomes <tidgomes@gmail.com>
2025-02-06 12:38:00 -0300

    fix: error that prevented tests to run properly

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,6 +1,11 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.test.json",
+    },
+  },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
     "\\.(css|scss|sass|less)$": "identity-obj-proxy",
